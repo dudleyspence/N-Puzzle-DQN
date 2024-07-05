3c490cd5610a9ffcd0402628bba0eaf0a951b861
dudleyspence <dudleyspence5@gmail.com>
2024-07-05 09:47:36 +0200

    Update Training_Loop.py

--- src/Training_Loop.py
+++ src/Training_Loop.py
@@ -16,18 +16,7 @@ import math
 import os
 #os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
-# Check for GPU availability
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.list_logical_devices('GPU')
-        print(f"{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs")
-    except RuntimeError as e:
-        print(e)
-else:
-    print("No GPUs found, using CPU.")
+
 
 
 def collect_gameplay_experiences(env, agent, buffer):

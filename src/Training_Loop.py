c068efab064abb12cabd4afc66fe675fad662fc6
dudleyspence <dudleyspence5@gmail.com>
2024-07-07 10:42:48 +0200

    Update Training_Loop.py

--- src/Training_Loop.py
+++ src/Training_Loop.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 import tensorflow as tf
 import math
 import os
+from collections import deque
 #os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'

502e2cd0d95f13eb000a07dbbbe27bb8c5b47d9f
dudleyspence <dudleyspence5@gmail.com>
2024-07-08 09:45:36 +0200

    Update Training_Loop.py

--- src/Training_Loop.py
+++ src/Training_Loop.py
@@ -183,5 +183,5 @@ else:
 
 # To further optimize the hyper-parameters change the function parameters below
 train_model(max_episodes=100000, n=n, batch_size=100, start_difficulty=start_difficulty,
-            final_epsilon=0.25, initial_epsilon=initial_epsilon, nodes=nodes, gamma=0.9, buffer_size=100000,
+            final_epsilon=0.25, initial_epsilon=initial_epsilon, nodes=nodes, gamma=0.99, buffer_size=100000,
             learning_rate=0.0001, time_steps=50)
